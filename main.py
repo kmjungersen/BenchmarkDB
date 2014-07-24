9b0ac449940d13dbd4db6fe8c27c400edd814bee
Kurtis Jungersen <kmjungersen@gmail.com>
2014-07-24 16:36:40 -0400

    continuing architecture

--- main.py
+++ main.py
@@ -1,3 +1,38 @@
 """
 Main.py will house all of the benchmarking commands
 """
+
+from mongo import BenchmarkMongo as Mongo
+
+import time
+import string
+import random
+
+
+class Benchmark():
+
+    def __init__(self):
+
+        self.entry_length = 10
+
+        pass
+
+    def random_entry(self, type='string'):
+
+        entry = ''
+
+        if type == 'string':
+
+            selection = string.ascii_letters
+
+        else:
+
+            selection = string.digits
+
+        for x in range(self.entry_length):
+
+            entry += random.choice(selection)
+
+        return entry
+
+
