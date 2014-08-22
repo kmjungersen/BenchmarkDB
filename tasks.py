ea6a4c3b734e2b35f668aea4302713bf52678020
kmjungersen <kmjungersen@gmail.com>
2014-08-22 11:57:56 -0400

    added more invoke tasks

--- tasks.py
+++ tasks.py
@@ -1,40 +1,76 @@
-import os
-import sys
-import importlib
+"""
+
+"""
+
 
 from invoke import task, run
 
-docs_dir = 'docs'
-build_dir = os.path.join(docs_dir, '_build')
+#TODO - make a docs directory
+
+
+def check_module_naming(name):
+    """ Checks to see if the inputted name is in the proper format, and fixes
+     it if not before returning it
+
+    :return name: The properly formatted DB module name
+    """
+
+    if not name.endswith('db'):
+        name += 'db'
+
+    return name
 
 
 @task
-def docs(clean=False, browse=False):
-    if clean:
-        clean_docs()
-    run("sphinx-build {0} {1}".format(docs_dir, build_dir), pty=True)
-    if browse:
-        browse_docs()
+def help():
+    """ Returns some basic task information, much of which provided by invoke """
 
-@task
-def browse_docs():
-    run("open {}".format(os.path.join(build_dir, 'index.html')))
+    run('invoke -l')
 
-@task
-def clean_docs():
-    run("rm -rf {dir}".format(dir=build_dir))
 
 @task
 def list_mods():
+    """ Returns a list of existing modules """
+
     from main import retrieve_module_list
 
     mod_list = retrieve_module_list()
 
     print mod_list
 
+
 @task
-def benchmarks(database):
+def benchmark(database):
+    """ Executes benchmarks with the default settings for a given DB
+    Usage: `invoke benchmark <database> """
+
+    database = check_module_naming(database)
 
     run("python main.py {db}".format(db=database))
 
-#TODO - add tasks for running ansible playbooks from invoke
+
+@task
+def vagrant_up(module):
+    """ Runs `vagrant up` for the specified module """
+
+    module = check_module_naming(module)
+
+    run("cd {mod}/ansible && vagrant up".format(mod=module))
+
+
+@task
+def install_ssh_copy_id():
+    """ Installs ssh_copy_id for mac """
+
+    run("curl -L https://raw.githubusercontent.com/beautifulcode/"
+        "ssh-copy-id-for-OSX/master/install.sh | sh")
+
+
+@task
+def deploy(database):
+    """ Runs the ansible playbook for a given db """
+
+    database = check_module_naming(database)
+
+    run('cd {db}/ansible && ansible-playbook -u vagrant -i hosts -s'
+        ' site.yml -vv'.format(db=database))
