1677c59450b444a15121f363546a2d73a69dd216
kmjungersen <kmjungersen@gmail.com>
2014-08-22 13:43:46 -0400

    modified mongo IPs

--- mongodb/local.py
+++ mongodb/local.py
@@ -2,10 +2,10 @@
 All settings for the mongodb module
 """
 
-MONGO_1 = '192.168.33.21'
-MONGO_2 = '192.168.33.22'
-MONGO_3 = '192.168.33.23'
-MONGO_4 = '192.168.33.24'
+MONGO_1 = '192.168.33.11'
+MONGO_2 = '192.168.33.12'
+MONGO_3 = '192.168.33.13'
+MONGO_4 = '192.168.33.14'
 
 MONGO_PRIMARY = MONGO_1
 
