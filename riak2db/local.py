4c1d63e148e4e63efa43c373a7e185646c08feef
kmjungersen <kmjungersen@gmail.com>
2014-11-13 18:00:03 -0500

    Changed host IP's

--- riak2db/local.py
+++ riak2db/local.py
@@ -3,9 +3,9 @@ All settings for Riak 2.0 database module
 """
 
 # List all of the IP addresses of nodes you would like to use for benchmarking
-RIAK_1 = '192.168.33.21'
-RIAK_2 = '192.168.33.22'
-RIAK_3 = '192.168.33.23'
+RIAK_1 = '192.168.33.31'
+RIAK_2 = '192.168.33.32'
+RIAK_3 = '192.168.33.33'
 
 # Port for connecting with PBC protocol
 RIAK_PORT = 8087
